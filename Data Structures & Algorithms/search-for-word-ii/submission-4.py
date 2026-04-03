53fc2682b89c74c9fc1e374e2aa936de264feb57
BillS <10003477+constructor-s@users.noreply.github.com>
2026-04-03 18:27:33 -0400

    Add: search-for-word-ii - submission-4

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-4.py
@@ -0,0 +1,43 @@
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        # Build a trie using just dict
+        # refs is impelemented for pruning
+        root = {}
+        for word in words:
+            curr = root
+            for c in word:
+                if c not in curr:
+                    curr[c] = {"refs": 1}
+                else:
+                    curr[c]["refs"] += 1
+                curr = curr[c]
+            curr["word"] = word
+
+        def search(board, r, c, root, visited):
+            """Recursively DFS the board, collect words reached."""
+            if not board or (r, c) in visited or r < 0 or r >= len(board) or c < 0 or c >= len(board[0]):
+                return []
+            if board[r][c] not in root or root[board[r][c]]["refs"] == 0:
+                return []
+
+            # board[r][c] is a possible path
+            visited = set(visited)
+            visited.add((r, c))
+
+            result = []
+            if "word" in root[board[r][c]]:
+                result.append(root[board[r][c]]["word"])
+            result.extend(search(board, r-1, c, root[board[r][c]], visited))
+            result.extend(search(board, r+1, c, root[board[r][c]], visited))
+            result.extend(search(board, r, c-1, root[board[r][c]], visited))
+            result.extend(search(board, r, c+1, root[board[r][c]], visited))
+
+            root[board[r][c]]["refs"] -= len(result)
+            return result
+        
+        # Start search from any cell on the board
+        result = set()
+        for r in range(len(board)):
+            for c in range(len(board[0])):
+                result.update(search(board, r, c, root, {}))
+        return list(result)
