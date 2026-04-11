7ea27ef0dacf1bfe65a4cf3bb201f897b196418f
BillS <10003477+constructor-s@users.noreply.github.com>
2026-04-11 02:57:14 -0400

    Add: coin-change-ii - submission-1

---
+++ Data Structures & Algorithms/coin-change-ii/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        if len(coins) == 0:
+            return 0
+
+        opt = [0] * (amount + 1) # opt for the current c
+        # Initialize with the first coin
+        c = coins[0]
+        for i in range(0, amount + 1, c):
+            opt[i] = 1
+        
+        for i in range(1, len(coins)): # add one possibility at a time
+            c = coins[i]
+            for i in range(c, amount + 1):
+                opt[i] = opt[i] + opt[i - c]
+
+            # print(opt)
+            # print(opt_new)
+            # print()
+
+        return opt[amount]
