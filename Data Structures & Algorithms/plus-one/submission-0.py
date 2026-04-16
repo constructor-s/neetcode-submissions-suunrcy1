108a54cd62c4c7a66ca2fbcf92cbf526b20d99d5
BillS <10003477+constructor-s@users.noreply.github.com>
2026-04-16 11:40:23 -0400

    Add: plus-one - submission-0

---
+++ Data Structures & Algorithms/plus-one/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        i = len(digits) - 1
+        if digits[i] != 9:
+            digits[i] += 1
+            return digits
+        else:
+            digits[i] = 0
+            carry = 1
+        
+        i -= 1
+        while carry and i >= 0:
+            if digits[i] != 9:
+                digits[i] += 1
+                return digits
+            else:
+                digits[i] = 0
+                carry = 1
+            i -= 1
+        
+        if i == -1:
+            digits.insert(0, 1)
+        return digits
