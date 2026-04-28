c7fed0c1c8e1616324b0ddc88e4a4d2dc197eac9
BillS <10003477+constructor-s@users.noreply.github.com>
2026-04-27 23:42:14 -0400

    Add: meeting-schedule - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule/submission-3.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        if (intervals.size() <= 1) return true;
+
+        std::sort(intervals.begin(), intervals.end(), [](const auto& a, const auto& b){
+            return a.start < b.start;
+        });
+        
+        // Searches the range [first, last) for two consecutive "true" elements. 
+        // An iterator to the first of the first pair of "true" elements, that is, 
+        // the first iterator it such that p(*it, *(it + 1)) != false. 
+        // If no such elements are found, last is returned. 
+        auto it = std::adjacent_find(intervals.begin(), intervals.end(),[](const auto& a, const auto& b){
+            return a.end > b.start;
+        });
+        return it == intervals.end();
+    }
+};
