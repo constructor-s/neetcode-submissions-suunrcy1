ea9c2ea54e962167ff6dc09b295f31c0d87b36b3
BillS <10003477+constructor-s@users.noreply.github.com>
2026-05-23 23:39:29 -0400

    Add: generate-parentheses - submission-8

---
+++ Data Structures & Algorithms/generate-parentheses/submission-8.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        if (n == 0) return {""};
+
+        std::vector<std::string> res;
+
+        auto init = std::make_pair("("s, 1);
+        std::stack<decltype(init)> sta;
+        sta.push(init);
+        while (!sta.empty()) {
+            auto [s, l_count] = std::move(sta.top());
+            sta.pop();
+            const auto r_count = s.size() - l_count;
+
+            if (l_count == n && r_count == n) res.push_back(std::move(s));
+            else {
+                if (l_count < n) sta.push({s + '(', l_count + 1});
+                if (l_count > r_count) sta.push({s + ')', l_count});
+            }
+        }
+
+        return res;
+    }
+};
