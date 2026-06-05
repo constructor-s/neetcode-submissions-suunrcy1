cf64d52b4cedce73a1e839afcf85a0942bd281cd
BillS <10003477+constructor-s@users.noreply.github.com>
2026-06-05 02:54:38 -0400

    Add: burst-balloons - submission-2

---
+++ Data Structures & Algorithms/burst-balloons/submission-2.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int maxCoins(vector<int>& nums) {
+        std::vector<int> nums1(nums.size() + 2);
+        nums1[0] = nums1[nums.size() + 1] = 1;
+        auto it1 = nums1.begin();
+        ++it1;
+        for (auto it = nums.begin(); it != nums.end(); ++it) {
+            *it1 = *it;
+            ++it1;
+        }
+        std::vector<std::vector<int>> cache(nums.size() + 2, std::vector<int>(nums.size() + 2, -1));
+
+        // for (auto it = nums1.begin(); it != nums1.end(); ++it) {
+        //     std::cout << *it << ' ';
+        // }
+        // std::cout << '\n';
+
+        return _maxCoins(
+            1, nums.size(),
+            nums1,
+            cache
+        );
+    }
+private:
+    int _maxCoins(int l, int r, const vector<int> &nums, std::vector<std::vector<int>> &cache) {
+        if (l > r) return 0;
+        if (cache[l][r] != -1) return cache[l][r];
+
+        int maxScore = 0;
+        for (int m = l; m <= r; ++m) {
+            int score = _maxCoins(l, m - 1, nums, cache) + nums[l - 1] * nums[m] * nums[r + 1] + _maxCoins(m + 1, r, nums, cache);
+            if (score > maxScore) maxScore = score;
+        }
+        cache[l][r] = maxScore;
+        // std::cout << l << ' ' << r << " => " << maxScore << '\n';
+        return maxScore;
+    }
+};
