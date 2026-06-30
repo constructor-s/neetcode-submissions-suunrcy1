fb12b400cabd24aa422cc17a04dd1dd8a3c9934e
BillS <10003477+constructor-s@users.noreply.github.com>
2026-06-30 02:21:39 -0400

    Add: count-connected-components - submission-0

---
+++ Data Structures & Algorithms/count-connected-components/submission-0.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int countComponents(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> adj(n);
+        for (auto e : edges) {
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
+        }
+        vector<bool> visited(n, false);
+        int components = 0;
+        for (int i = 0; i < n; ++i) {
+            if (!visited[i]) {
+                dfs(i, adj, visited);
+                ++components;
+            }
+        }
+        return components;
+    }
+
+    void dfs(int i, const vector<vector<int>>& adj, vector<bool>& visited) {
+        stack<int> s;
+        s.push(i);
+        // cout << "start " << i << '\n';
+        while (s.size()) {
+            auto i = s.top();
+            // cout << "found " << i << '\n';
+            s.pop();
+            visited[i] = true;
+            for (auto e : adj[i]) {
+                if (!visited[e]) {
+                    s.push(e);
+                }
+            }
+        }
+    }
+};
